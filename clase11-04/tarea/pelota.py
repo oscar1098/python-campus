5716fb26f06a03007484f7ba6541bc65e1e0028f
aaa <aaa>
2023-04-12 14:40:14 -0500

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	new file:   clase11-04/tarea/pelota.py
    	new file:   clase11-04/tarea/sumatoriaCero.py

---
+++ clase11-04/tarea/pelota.py
@@ -0,0 +1,10 @@
+altura = 10
+rebote = 0
+
+while altura > 0.5:
+    rebote +=1
+    altura *= 0.9
+
+print(rebote)
+    
+    
---
+++ clase11-04/tarea/sumatoriaCero.py
@@ -0,0 +1,8 @@
+n = int(input('Ingrese un numero '))
+sum = 0
+
+while n != 0:
+    sum +=n
+    n = int(input('Ingrese un numero '))
+    
+print(sum)
