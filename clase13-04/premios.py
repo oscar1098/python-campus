607cf7d592f8ae7f76e24f01efc17627c006adfe
oscar1098 <oscarardila1098@gmail.com>
2023-04-16 20:59:15 -0500

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   clase13-04/json/dicAvanzado.json
    	modified:   clase13-04/json/dicBasico.json
    	modified:   clase13-04/json/dicIntermedio.json
    	modified:   clase13-04/json/trainers.json
    	modified:   clase13-04/premios.py

--- clase13-04/premios.py
+++ clase13-04/premios.py
@@ -5,7 +5,6 @@ rutaBasico = './clase13-04/json/dicBasico.json'
 rutaIntermedio = './clase13-04/json/dicIntermedio.json'
 rutaAvanzado = './clase13-04/json/dicAvanzado.json'
 rutaTrainer = './clase13-04/json/trainers.json'
-menuGeneral = 0
 arregloTrainer = ['','','']
 
 def leerJson(ruta):
@@ -24,59 +23,65 @@ def generearId(diccionario):
         id = int(list(diccionario.keys())[-1])+1
         return id
     except IndexError:
-        id = 1
-        return id
+        return 1
+
+def revisarJson(ruta):
+    try : 
+        leerJson(ruta)
+    except FileNotFoundError:
+        actualizarJson(ruta)
 
 def menu():
     os.system('clear')
-    print('Seleccione:\n1.Registrar Campers\n2.Registrar Expert Trainer\n3.Mostrar campers por grupo\n4.Mostar campers de mayor y menor edad\n5.Salir\n')
-    menuGeneral = input()
+    menuGeneral = input('Seleccione:\n1.Registrar Campers\n2.Registrar Expert Trainer\n3.Mostrar campers por grupo\n4.Mostar campers de mayor y menor edad\n5.Salir\n')
+
     return menuGeneral
 
 def menuRegistrarCampers():
     os.system('clear')
     print('*********************REGISTRAR CAMPERS*********************')
-    print('Seleccione a que grupo desea añadir campers:\n1.Basico\n2.Intermedio\n3.Avanzado\n4.Salir')
-    menuRegistrarCampers = input()
+    menuRegistrarCampers = input('Seleccione a que grupo desea añadir campers:\n1.Basico\n2.Intermedio\n3.Avanzado\n4.Salir\n')
+
     return menuRegistrarCampers
     
-def registrarCampers(team,id,diccionario,numero,ruta):
+def registrarCampers(team,ruta):
     os.system('clear')
-    print('Cuantos estudiantes desea agregar al grupo',team)
+    print('Cuantos estudiantes desea agregar al grupo\n')
     totalGrupo = int(input())
+
+    diccionario = leerJson(ruta)
+    id = generearId(diccionario)
 
     for i in range(totalGrupo):
         nombre = input('\nIngrese el nombre ')
         mesIngreso = input('Ingrese la fecha (Ejempo: Enero) ')
         edad = input('Ingrese la edad ')
-        diccionario[id]={
-            'nombre' : nombre,
-            'mesIngreso' : mesIngreso,
-            'grupo' : team,
-            'edad' : edad
-        }
+        diccionario[id]={'nombre' : nombre,'mesIngreso' : mesIngreso,'grupo' : team,'edad' : edad}
         id +=1
-    match numero:
-        case 1:
-            actualizarJson(ruta,diccionario)
-        case 2:
-            actualizarJson(ruta,diccionario)
-        case 3:
-            actualizarJson(ruta,diccionario)
+    actualizarJson(ruta,diccionario)
 
-def registrarTrainer():
+def MenuregistrarTrainer():
     os.system('clear')
     print('*********************REGISTRAR EXPERT TRAINER*********************')
-    print('\nSeleccione:\n1.Expert trainer grupo basico\n2.Expert trainer grupo intermedio\n3.Expert trainer grupo avanzado')
-    trainer = input()
+    trainer = input('\nSeleccione:\n1.Expert trainer grupo basico\n2.Expert trainer grupo intermedio\n3.Expert trainer grupo avanzado\n')
+
     return trainer
 
-def mostrarEstudiante(diccionario,arreglo,i):
+def registrarTrainer(i,ruta):
+    arregloTrainer = leerJson(ruta) 
+    trainer = input('Ingrese el nombre del trainer ')
+    arregloTrainer[i] = trainer
+    actualizarJson(ruta,arregloTrainer)
+
+def mostrarEstudiante(rutaEstu,rutaTrainer,i):
     os.system('clear')
-    print('El trainer del grupo es ',arreglo[i],'\n')
+    diccionario = leerJson(rutaEstu)
+    trainer = leerJson(rutaTrainer)
+    print('El trainer del grupo es ',trainer[i],'\n')
     print('ESTUDIANTE\t\tMES DE INGRESO\t\tEDAD\n')
     for estudiante in diccionario:
         print(diccionario[estudiante]['nombre'],'\t\t',diccionario[estudiante]['mesIngreso'],'\t\t\t',diccionario[estudiante]['edad'])
+    input()
 
 def mayorEdadMenorEdad(diccionario):
     mayor = ['0','']
@@ -89,26 +94,26 @@ def mayorEdadMenorEdad(diccionario):
             menor[0] = diccionario[estudiante]['edad']
             menor[1] = diccionario[estudiante]['nombre']
     mayorMenorEdad = [mayor,menor]
+
     return mayorMenorEdad
 
-try : 
-    leerJson(rutaBasico)
-except FileNotFoundError:
-    actualizarJson(rutaBasico)
+def mostrarEdades(ruta):
+    diccionario = leerJson(ruta)
+    edades = mayorEdadMenorEdad(diccionario)
+    os.system('clear')
+    print('Estudiante de mayor edad es',edades[0][1],'con ',edades[0][0],' años')
+    print('Estudiante de menor edad es',edades[1][1],'con ',edades[1][0],' años')
+    input()
 
-try : 
-    leerJson(rutaIntermedio)
-except FileNotFoundError:
-    actualizarJson(rutaIntermedio)
+revisarJson(rutaBasico)
 
-try : 
-    leerJson(rutaAvanzado)
-except FileNotFoundError:
-    actualizarJson(rutaAvanzado)
+revisarJson(rutaIntermedio)
+
+revisarJson(rutaAvanzado)
 
 try:
     leerJson(rutaTrainer)
-except FileExistsError:
+except FileNotFoundError:
     actualizarJson(rutaTrainer,arregloTrainer)
     
 menuGeneral = menu() 
@@ -120,86 +125,45 @@ while menuGeneral != '5':
             while menuRegisCampers != '4':
                 match menuRegisCampers:
                     case '1':
-                        diccionarioBasico = leerJson(rutaBasico)
-                        id = generearId(diccionarioBasico)
-                        registrarCampers('Basico',id,diccionarioBasico,1,rutaBasico)
+                        registrarCampers('Basico',rutaBasico)
                     case '2':
-                        diccionarioIntermedio = leerJson(rutaIntermedio)
-                        id = generearId(diccionarioIntermedio)
-                        registrarCampers('Intermedio',id,diccionarioIntermedio,2,rutaIntermedio)
+                        registrarCampers('Intermedio',rutaIntermedio)
                     case '3':
-                        diccionarioAvanzado = leerJson(rutaAvanzado)
-                        id = generearId(diccionarioAvanzado)
-                        registrarCampers('Avanzado',id,diccionarioAvanzado,3,rutaAvanzado)
+                        registrarCampers('Avanzado',rutaAvanzado)
                     case other:
                         print('Seleccion Invalida')
                 menuRegisCampers = menuRegistrarCampers()
         case '2':
-            trainerMenu = registrarTrainer()
+            trainerMenu = MenuregistrarTrainer()
             match trainerMenu:
                 case '1':
-                    arregloTrainer = leerJson(rutaTrainer) 
-                    trainerBasico = input('Ingrese el nombre del trainer Basico ')
-                    arregloTrainer[0] = trainerBasico
-                    actualizarJson(rutaTrainer,arregloTrainer)
+                    registrarTrainer(0,rutaTrainer)
                 case '2': 
-                    arregloTrainer = leerJson(rutaTrainer)
-                    trainerIntermedio = input('Ingrese el nombre del trainer Intermedio ')
-                    arregloTrainer[1] = trainerIntermedio
-                    actualizarJson(rutaTrainer,arregloTrainer)
+                    registrarTrainer(1,rutaTrainer)
                 case '3': 
-                    arregloTrainer = leerJson(rutaTrainer)
-                    trainerAvanzado = input('Ingrese el nombre del trainer avanzado ')
-                    arregloTrainer[2] = trainerAvanzado
-                    actualizarJson(rutaTrainer,arregloTrainer)
-                
+                    registrarTrainer(2,rutaTrainer)
         case '3':
             os.system('clear')
             print('*********************LISTAR CAMPERS*********************')
-            mostrar = input('Seleccione\n1.Grupo basico\n2.Grupo intermedio\n3.Grupo avanzado ')
+            mostrar = input('Seleccione\n1.Grupo basico\n2.Grupo intermedio\n3.Grupo avanzado\n')
             match mostrar:
                 case '1':
-                    diccionario = leerJson(rutaBasico)
-                    trainer = leerJson(rutaTrainer)
-                    mostrarEstudiante(diccionario,trainer,0)
-                    input()
+                    mostrarEstudiante(rutaBasico,rutaTrainer,0)
                 case '2':
-                    diccionario = leerJson(rutaIntermedio)
-                    trainer = leerJson(rutaTrainer)
-                    mostrarEstudiante(diccionario,trainer,1)
-                    input()
+                    mostrarEstudiante(rutaIntermedio,rutaTrainer,1)
                 case '3':
-                    diccionario = leerJson(rutaAvanzado)
-                    trainer = leerJson(rutaTrainer)
-                    mostrarEstudiante(diccionario,trainer,2)
-                    input()
-
+                    mostrarEstudiante(rutaAvanzado,rutaTrainer,2)
         case '4':
             os.system('clear')
             print('*********************MOSTRAR CAMPERS DE MAYOR Y MENOR EDAD*********************')
             grupo = input('Seleccione\n1.Grupo basico\n2.Grupo intermedio\n3.Grupo avanzado\n ')
             match grupo:
                 case '1':
-                    diccionarioBasico = leerJson(rutaBasico)
-                    edades = mayorEdadMenorEdad(diccionarioBasico)
-                    os.system('clear')
-                    print('Estudiante de mayor edad ',edades[0][1],'con ',edades[0][0],' años')
-                    print('Estudiante de menor edad ',edades[1][1],'con ',edades[1][0],' años')
-                    input()
+                    mostrarEdades(rutaBasico)
                 case '2':
-                    diccionarioIntermedio = leerJson(rutaIntermedio)
-                    edades = mayorEdadMenorEdad(diccionarioIntermedio)
-                    os.system('clear')
-                    print('Estudiante de mayor edad ',edades[0][1],'con ',edades[0][0],' años')
-                    print('Estudiante de menor edad ',edades[1][1],'con ',edades[1][0],' años')
-                    input()
+                    mostrarEdades(rutaIntermedio)
                 case '3':
-                    diccionarioAvanzado = leerJson(rutaAvanzado)
-                    edades = mayorEdadMenorEdad(rutaAvanzado)
-                    os.system('clear')
-                    print('Estudiante de mayor edad ',edades[0][1],'con ',edades[0][0],' años')
-                    print('Estudiante de menor edad ',edades[1][0],'con ',edades[1][1],' años')
-                    input()
+                    mostrarEdades(rutaAvanzado)
         case other:
             print('Opcion invalida')
     menuGeneral = menu()
